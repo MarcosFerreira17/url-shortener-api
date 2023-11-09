da2d77b4b9e60f8372070800154a284f981e4ba0
Marcos Ferreira <marcosfw7@outlook.com>
2023-11-09 09:48:56 -0300

    update docker

--- src/UrlShortener.Application/Services/CacheService.cs
+++ src/UrlShortener.Application/Services/CacheService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
 using UrlShortener.Application.Interfaces;
@@ -8,21 +9,32 @@ public class CacheService : ICacheService
     private readonly IDistributedCache _cache;
     public CacheService(IDistributedCache cache)
     {
-        _cache = cache;
+        _cache = cache ?? throw new ArgumentNullException(nameof(cache));
     }
 
-    public Task<T> GetOrSetAsync<T>(string key, Func<Task<T>> func, TimeSpan? absoluteExpiration = null)
+    public async Task<T> GetOrSetAsync<T>(string key, Func<Task<T>> func, TimeSpan? absoluteExpiration = null)
     {
-        var cached = _cache.GetString(key);
+        var cached = await _cache.GetAsync(key);
+
+        string serializedCache;
 
         if (cached != null)
-            return Task.FromResult(JsonSerializer.Deserialize<T>(cached));
-
-        var result = func();
-
-        _cache.SetString(key, JsonSerializer.Serialize(result), new DistributedCacheEntryOptions
         {
-            AbsoluteExpirationRelativeToNow = absoluteExpiration is null ? TimeSpan.FromMinutes(5) : absoluteExpiration
+            serializedCache = Encoding.UTF8.GetString(cached);
+
+            return JsonSerializer.Deserialize<T>(serializedCache);
+        }
+
+        var result = await func();
+
+        serializedCache = JsonSerializer.Serialize(result);
+
+        byte[] byteCache = Encoding.UTF8.GetBytes(serializedCache);
+
+        await _cache.SetAsync(key, byteCache, new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpiration ?? TimeSpan.FromMinutes(5),
+            SlidingExpiration = TimeSpan.FromMinutes(2),
         });
 
         return result;
